a2cbbdab0569bdb4deafef8636f18e99be837ae2
Christian Dupuis <cd@atomist.com>
2017-10-01 23:31:26 +0200

    fix message sending for unmapped pushes

--- src/events/NotifyOnPush.ts
+++ src/events/NotifyOnPush.ts
@@ -1,13 +1,16 @@
 import * as GraphQL from "@atomist/automation-client/graph/graphQL";
+import { Failure } from "@atomist/automation-client/HandlerResult";
 import {
     EventFired,
     EventHandler,
     HandleEvent,
     HandlerContext,
     HandlerResult,
+    Success,
     Tags,
 } from "@atomist/automation-client/Handlers";
 import { logger } from "@atomist/automation-client/internal/util/logger";
+import * as _ from "lodash";
 import * as graphql from "../typings/types";
 
 @EventHandler("Notify channel on push", GraphQL.subscriptionFromFile("graphql/push"))
@@ -17,11 +20,15 @@ export class NotifyOnPush implements HandleEvent<graphql.PushWithRepo.Subscripti
     public handle(e: EventFired<graphql.PushWithRepo.Subscription>, ctx: HandlerContext): Promise<HandlerResult> {
         logger.info(`Incoming event is %s`, JSON.stringify(e.data, null, 2));
 
-        return Promise.all(e.data.Push.map(p =>
-            ctx.messageClient.addressChannels(`Got a push with sha \`${p.after.sha}\``,
-                p.repo.channels.map(c => c.name))))
-            .then(() => {
-                return Promise.resolve({ code: 0 });
-            });
+        return Promise.all(e.data.Push.map(p => {
+                const channels = _.get(p, "repo.channels") as graphql.PushWithRepo.Channels[];
+
+                if (channels && channels.length > 0) {
+                    ctx.messageClient.addressChannels(`Got a push with sha \`${p.after.sha}\``,
+                        channels.map(c => c.name));
+                }
+            }))
+            .then(() => Success)
+            .catch(() => Failure);
     }
 }

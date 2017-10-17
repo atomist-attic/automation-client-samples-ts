e1d2d12228b26ac81da9e0c217fb9cb4c818dd46
Jessica Kerr <jessitron@gmail.com>
2017-10-17 16:55:08 -0500

    Comment only on issues created by you

--- src/events/CommentOnIssue.ts
+++ src/events/CommentOnIssue.ts
@@ -6,6 +6,7 @@ import {
     HandlerResult,
     Secret,
     Secrets,
+    Success,
     Tags,
 } from "@atomist/automation-client";
 import axios from "axios";
@@ -15,6 +16,9 @@ import axios from "axios";
   Issue {
     number
     title
+    openedBy {
+        login
+    }
     repo {
       owner
       name
@@ -32,17 +36,37 @@ export class CommentOnIssue implements HandleEvent<any> {
 
     public handle(e: EventFired<any>, ctx: HandlerContext): Promise<HandlerResult> {
         const issue = e.data.Issue[0];
+        const authorizingGithubUser = whoami(this.githubToken);
 
-        return ctx.messageClient.addressChannels(`Got a new issue \`${issue.number}# ${issue.title}\``,
-            issue.repo.channels.map(c => c.name))
-            .then(() => {
-                // tslint:disable-next-line:max-line-length
-                return axios.post(`https://api.github.com/repos/${issue.repo.owner}/${issue.repo.name}/issues/${issue.number}/comments`,
-                    { body: "Hey, I saw your issue!" },
-                    { headers: { Authorization: `token ${this.githubToken}` } });
-            })
-            .then(() => {
-                return Promise.resolve({ code: 0 });
-            });
+        // To see these messages, configure Atomist in any slack channel: `@atomist-bot repo <repository-name>` 
+        // Then create an issue in <repository-name> to see this notification in that channel!
+        const postInLinkedChannels = ctx.messageClient.addressChannels(`Got a new issue \`${issue.number}# ${issue.title}\``,
+            issue.repo.channels.map(c => c.name));
+
+        const commentOnIssue = postInLinkedChannels
+            .then(() =>
+                authorizingGithubUser.then(me => {
+                    // Only comment on issues that I created. Take out this condition to comment on all issues!
+                    if (issue.openedBy.login === me)
+                        // tslint:disable-next-line:max-line-length
+                        return axios.post(`https://api.github.com/repos/${issue.repo.owner}/${issue.repo.name}/issues/${issue.number}/comments`,
+                            { body: "Hey, I saw your issue!" },
+                            { headers: { Authorization: `token ${this.githubToken}` } });
+                    else {
+                        console.log(`This issue was created by ${issue.openedBy.login} and I am ${me} so I will not comment on it.`);
+                        return;
+                    }
+                }));
+        return commentOnIssue.then(() => {
+            return Success;
+        });
     }
+}
+
+function whoami(githubToken: string): Promise<string> {
+    const githubUser = axios.get(
+        `https://api.github.com/user`,
+        { headers: { Authorization: `token ${githubToken}` } })
+        .then(response => response.data.login);
+    return githubUser;
 }

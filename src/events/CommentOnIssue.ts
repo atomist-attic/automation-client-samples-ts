7c1a7267ff0881935f4682784a9ce5e403b1b7ff
Jessica Kerr <jessitron@gmail.com>
2017-10-17 16:58:59 -0500

    lint

--- src/events/CommentOnIssue.ts
+++ src/events/CommentOnIssue.ts
@@ -38,21 +38,23 @@ export class CommentOnIssue implements HandleEvent<any> {
         const issue = e.data.Issue[0];
         const authorizingGithubUser = whoami(this.githubToken);
 
-        // To see these messages, configure Atomist in any slack channel: `@atomist-bot repo <repository-name>` 
+        // To see these messages, configure Atomist in any slack channel: `@atomist-bot repo <repository-name>`
         // Then create an issue in <repository-name> to see this notification in that channel!
-        const postInLinkedChannels = ctx.messageClient.addressChannels(`Got a new issue \`${issue.number}# ${issue.title}\``,
-            issue.repo.channels.map(c => c.name));
+        const postInLinkedChannels =
+            ctx.messageClient.addressChannels(`Got a new issue \`${issue.number}# ${issue.title}\``,
+                issue.repo.channels.map(c => c.name));
 
         const commentOnIssue = postInLinkedChannels
             .then(() =>
                 authorizingGithubUser.then(me => {
                     // Only comment on issues that I created. Take out this condition to comment on all issues!
-                    if (issue.openedBy.login === me)
+                    if (issue.openedBy.login === me) {
                         // tslint:disable-next-line:max-line-length
                         return axios.post(`https://api.github.com/repos/${issue.repo.owner}/${issue.repo.name}/issues/${issue.number}/comments`,
                             { body: "Hey, I saw your issue!" },
                             { headers: { Authorization: `token ${this.githubToken}` } });
-                    else {
+                    } else {
+                        // tslint:disable-next-line:max-line-length
                         console.log(`This issue was created by ${issue.openedBy.login} and I am ${me} so I will not comment on it.`);
                         return;
                     }

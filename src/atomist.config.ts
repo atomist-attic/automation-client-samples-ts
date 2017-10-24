cacbb3f340cc8c7e7097c14aa6cc3307bbad4d8d
David Dooling <david@atomist.com>
2017-10-24 08:19:55 -0500

    Simplify test log format, use user config
    Do not use syslog format for test logging.
    
    Remove bogus team from atomist.config.ts, allowing config to fall
    through to user config created by atomist-config.

--- src/atomist.config.ts
+++ src/atomist.config.ts
@@ -18,7 +18,7 @@ const token = process.env.GITHUB_TOKEN;
 export const configuration: Configuration = {
     name: pj.name,
     version: pj.version,
-    teamIds: "T01234567", // <-- run @atomist pwd in your slack team to obtain the team id
+    teamIds: [], // <-- run @atomist pwd in your slack team to obtain the team id
     commands: [
         HelloWorld,
         SpringBootVersionReviewer,
--- test/util/tempProject.ts
+++ test/util/tempProject.ts
@@ -2,6 +2,10 @@ import { LocalProject } from "@atomist/automation-client/project/local/LocalProj
 import { NodeFsLocalProject } from "@atomist/automation-client/project/local/NodeFsLocalProject";
 import * as tmp from "tmp";
 
+import { LoggingConfig } from "@atomist/automation-client/internal/util/logger";
+
+LoggingConfig.format = "cli";
+
 export function tempProject(): LocalProject {
     const dir = tmp.dirSync();
     return new NodeFsLocalProject("temp", dir.name);
